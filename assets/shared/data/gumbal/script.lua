9cf9b261d0cf81310ccc7a6e438a515ddb916656
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-03-09 18:38:43 -0300

    Add files via upload

---
+++ assets/shared/data/gumbal/script.lua
@@ -0,0 +1,124 @@
+local xx = 340; 
+local yy = 600; 
+
+local xx2 = 630; 
+local yy2 = 600; 
+
+local xx3 = 520;
+local yy3 = 450;
+
+local ofs = 35;
+local followchars = true; 
+local del = 0;
+local del2 = 0;
+
+
+local allowCountdown = false
+
+
+function onCreate()
+end
+
+function onUpdate() -- The Main Code
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then -- Code for the camera to follow the poses of your opponent
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if gfSection == true then -- The camera follows GF when she sings, only when the "GF Section" option in the chart editor is activated. 
+                if getProperty('gf.animation.curAnim.name') == 'singLEFT' then -- Credits to Serebeat and company for their Slaybells mod,
+                    triggerEvent('Camera Follow Pos',xx3-ofs,yy3)              -- That's where I got the gf code from.
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singRIGHT' then
+                    triggerEvent('Camera Follow Pos',xx3+ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singUP' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3-ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singDOWN' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3+ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'idle-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3)
+				end
+                if getProperty('gf.animation.curAnim.name') == 'singRIGHT-alt' then
+                    triggerEvent('Camera Follow Pos',xx3+ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singUP-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3-ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singDOWN-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3+ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'idle-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3)
+                end
+            end
+        else
+                 -- Code for the camera to follow the poses of boyfriend
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','') -- Self explanatory
+    end
+	
+
+end
+
+
